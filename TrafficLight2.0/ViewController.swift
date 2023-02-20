40c3261d0a48502f46f8931c8efbdc1373ce3b96
Viktor Teslenko <meex_v92@mail.ru>
2023-02-20 14:14:06 +0800

    Made new version

--- TrafficLight2.0/ViewController.swift
+++ TrafficLight2.0/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum CurrentColor {
+    case red, yellow, green
+    }
+    
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    private let currentColor = CurrentColor.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        redLight.layer.cornerRadius = redLight.frame.width/2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width/2
+        greenLight.layer.cornerRadius = greenLight.frame.width/2
     }
 
+    
+
+    
+    
 
 }
 
